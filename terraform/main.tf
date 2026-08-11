31dd16218edd607112e5b7db8a04024d4bf929e6
Jeysibn <jeysibn@gmail.com>
2026-08-11 15:44:45 +0800

    initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,8 +44,7 @@ resource "proxmox_virtual_environment_vm" "k3s_node" {
     }
     user_account {
       username = "ubuntu"
-      keys     = [var.ssh_public_key]
-      
+      keys     = [var.ssh_public_key] 
     }
   }
 }
